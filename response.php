b563bcdad7c15cf487a7aba7105ddb7b71295cc7
Your Name <you@example.com>
2021-05-14 16:25:42 -0300

    ..

--- response.php
+++ response.php
@@ -2,29 +2,8 @@
     require_once("src/ScrapController.php"); 
 
     if($_GET['code']){
-        echo '<div id="rastreio">';
-            $scrap = new Scrap($_GET['code']);
-        echo '</div>';
+        $scrap = new Scrap($_GET['code']);
     }else{
         echo '{"error": "nenhum objeto encontrado"}';
     }
 ?>
-
-<script>
-    let body = document.body;
-
-    let objeto = body.innerText.match(/Útimo Status do Objeto: (.*)/g)[0];
-    let status = body.innerText.match(/Status: (.*)/g)[0];
-    let date =  body.innerText.match(/Data : (.*)/g)[0];
-    let percurso = body.innerText.match(/De (.*)/g)[0];
-
-    let res = {
-                "código": objeto.replace('Útimo Status do Objeto: ', ''),
-                "status": status,
-                "date"  : date,
-                "percurso": percurso
-            }
-    
-    body.innerHTML = JSON.stringify(res);
-
-</script>
