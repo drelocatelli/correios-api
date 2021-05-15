96a8e3078132c21e4958a3d60333ccc5c0e4cfa7
Your Name <you@example.com>
2021-05-15 17:26:21 -0300

    ..

--- response.php
+++ response.php
@@ -1,9 +1,16 @@
 <?php 
     require_once("src/ScrapController.php"); 
 
-    if($_POST['code']){
+    if(isset($_POST['code'])){
+        scrap($_POST['code']);
+    }else if($_GET['code']){
+        scrap($_GET['code']);
+    }
+
+
+    function scrap($var){
         try{
-            $scrap = new Scrap($_POST['code']);
+            $scrap = new Scrap($var);
             $scrap->getSomething("All");
         }catch(Exception $e){
             echo "Erro: " . $e->getMessage();
